123bc6fa3146f97b59a90ba1687e4cc8f0b68f62
Joao Martins <JoaoMGWork@hotmail.com>
2023-02-12 11:32:50 +0000

    started

---
+++ react-course/src/App.js
@@ -0,0 +1,14 @@
+import Todo from './components/Todo'
+
+function App() {
+  return (
+    <div>
+      <h1>My todos</h1>
+      <Todo title='Learn React'/>
+      <Todo title='Master React'/>
+      <Todo title='Holy React'/>
+    </div>
+  )
+}
+
+export default App
